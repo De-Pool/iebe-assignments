178b5667176d4ba2c711dbae2f3a8b0e480c91fe
Daniel <d.c.Poolman@student.tudelft.nl>
2021-10-22 17:03:54 +0200

    Betere residual vs fitted values plot

--- src/main.py
+++ src/main.py
@@ -1,18 +1,19 @@
-# pip install pyreadr
 import pyreadr
-
-# pip install statsmodels
+import pandas as pd
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
 import statsmodels.stats.diagnostic as stats
 import pylab
-
+import seaborn as sns
 import numpy as np
 import numpy.linalg as linalg
 import matplotlib.pyplot as plt
 
 
 def main():
+    sns.set_theme(color_codes=True)
+    sns.set_theme(style="ticks")
+
     data = pyreadr.read_r('./data.RData')
     # nervlove_data has the following columns:
     # totcost, output, plabor, pfuel, pkap
@@ -33,19 +34,19 @@ def exercise1(data, print_all):
     ols_model = smf.ols(formula=formula, data=data)
     ols_model_fit = ols_model.fit()
 
-    # E[u'u|X] = I*sigma^squared --> show the presence of homoskedasticity
-    data['u'] = ols_model_fit.resid
-    # formula_residuals = 'u ~ log(output) + log(plabor) + log(pfuel) + log(pkap) - 1'
-    # ols_model_residuals = smf.ols(formula=formula_residuals, data=data).fit()
+    u_output_cov = np.cov(ols_model_fit.resid, log(data['output']))[0][1]
+    u_plabor_cov = np.cov(ols_model_fit.resid, log(data['plabor']))[0][1]
+    u_pfuel_cov = np.cov(ols_model_fit.resid, log(data['pfuel']))[0][1]
+    u_pkap_cov = np.cov(ols_model_fit.resid, log(data['pkap']))[0][1]
 
-    # Null hypothesis of the Breusch Pagan test is the presence of homoskedasticity
-    # if the pvalue is less than 0.05, we reject the null hypothesis,
-    # so we can conclude heteroskedasticity is present.
-    _, _, _, fpvalue = stats.het_breuschpagan(ols_model_fit.resid, ols_model_fit.model.exog)
     if print_all:
         print('Exercise 1')
         print(ols_model_fit.summary(), '\n')
-        print('p-value breusch pagan', fpvalue, '\n')
+        print('Covariance between u and output:', u_output_cov, np.nanmean(log(data['output'])))
+        print('Covariance between u and plabor:', u_plabor_cov, np.nanmean(log(data['plabor'])))
+        print('Covariance between u and pfuel:', u_pfuel_cov, np.nanmean(log(data['pfuel'])))
+        print('Covariance between u and pkap:', u_pkap_cov, np.nanmean(log(data['pkap'])))
+        print('Average of residuals', np.nanmean(ols_model_fit.resid.values), '\n')
 
     return ols_model
 
@@ -96,33 +97,42 @@ def exercise2(data, model, print_all):
         print('heteroskedasticity-robust standard error t-statistic\n', t_hc_robust, '\n')
         print('F-statistic', F, '\n')
 
-    return F
-
 
 def exercise3(model):
     model_fit = model.fit()
+    data = pd.DataFrame()
+    data['residuals'] = model_fit.resid.values
+    data['fitted'] = model_fit.fittedvalues.values
 
     # Plot the residuals against the fitted values
-    residuals = model_fit.resid.values
-    fitted_values = model_fit.fittedvalues.values
-    plt.scatter(fitted_values, residuals)
+    sns.lmplot(x="fitted", y="residuals", data=data, height=5, aspect=1.5, truncate=False, order=2)
+    plt.scatter(data['fitted'], data['residuals'])
     plt.title("Residuals plotted against the fitted values (Question 1 model)")
     plt.xlabel("Fitted values")
     plt.ylabel("Residuals")
     plt.show()
 
     # QQ plot
-    sm.qqplot(residuals, line='45')
+    sm.qqplot(data['residuals'], line='45')
     pylab.show()
+
+    # E[u'u|X] = I*sigma^squared --> show the presence of homoskedasticity
+    # Null hypothesis of the Breusch Pagan test is the presence of homoskedasticity
+    # if the pvalue is less than 0.05, we reject the null hypothesis,
+    # so we can conclude heteroskedasticity is present.
+    _, _, _, fpvalue = stats.het_breuschpagan(model_fit.resid, model_fit.model.exog)
+    print('p-value breusch pagan', fpvalue, '\n')
 
 
 def exercise4(data, model):
     model_fit = model.fit()
 
-    # Plot the residuals against the fitted values
-    residuals = model_fit.resid.values
-    log_q = log(data['output'])
-    plt.scatter(log_q, residuals)
+    data['residuals'] = model_fit.resid.values
+    data['log_q'] = log(data['output'])
+
+    # Plot the residuals against log(Q)
+    sns.lmplot(x="log_q", y="residuals", data=data, height=5, aspect=1.5, truncate=False, order=2)
+    plt.scatter(data['log_q'], data['residuals'])
     plt.title("Residuals plotted against log(Q) (Question 1 model)")
     plt.xlabel("log(Q)")
     plt.ylabel("Residuals")
@@ -136,17 +146,19 @@ def exercise5(data, print_all):
     ols_model = smf.ols(formula=formula, data=data)
     ols_model_fit = ols_model.fit()
 
+    data['residuals'] = ols_model_fit.resid.values
+    data['fitted'] = ols_model_fit.fittedvalues.values
+
     # Plot the residuals against the fitted values
-    residuals = ols_model_fit.resid.values
-    fitted_values = ols_model_fit.fittedvalues.values
-    plt.scatter(fitted_values, residuals)
+    sns.lmplot(x="fitted", y="residuals", data=data, height=5, aspect=1.5, truncate=False)
+    plt.scatter(data['fitted'], data['residuals'])
     plt.title("Residuals plotted against the fitted values (Question 5 model)")
     plt.xlabel("Fitted values")
     plt.ylabel("Residuals")
     plt.show()
 
     # QQ plot
-    sm.qqplot(residuals, line='45')
+    sm.qqplot(data['residuals'], line='45')
     pylab.show()
 
     if print_all:
